72dfde7ba76fa58d9841c0e698eb1f09b86e387b
drezi29 <julia.drezek98@gmail.com>
2024-06-12 18:05:59 +0200

    chore: database configuration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine
+import models
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 async def root():
